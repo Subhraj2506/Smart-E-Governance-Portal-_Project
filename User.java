302b78d59fae2d849612f4b67aa8a94da0f7bcea
Subhraj2506 <rajsubh2003@gmail.com>
2025-11-19 18:52:44 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,29 @@
+package com.egov.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    @NotBlank
+    @Email
+    private String email;
+}
